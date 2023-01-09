23b05d1d25855af9de07aa934bcfd9cdb44f0a44
Anup <anuproybca@gmail.com>
2023-01-09 09:17:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+This is my testing React App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h2>Anup Roy</h2>
       </header>
     </div>
   );
